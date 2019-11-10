1de589c12d7faaf5bc5c51fa7bab161460c60a3a
Mira Jambusaria <mira_jambusaria@yahoo.com>
2019-11-09 23:54:54 -0600

    added scene1 buttons Left and Right Sides

--- public/js/Scene1.js
+++ public/js/Scene1.js
@@ -1,6 +1,7 @@
 class Scene1 extends Phaser.Scene {
     constructor() {
         super("sideSelector");
+        this.selectedSide = 'rightSide'; 
     }
 
     init() {
@@ -8,15 +9,30 @@ class Scene1 extends Phaser.Scene {
     }
 
     create() {
-        this.scene.start("playGame");
+        this.clickCountText = this.add.text(10, 20, '');
+
+        this.leftButton = this.add.image(200, 300, 'Left')
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => {
+                this.selectedSide = 'leftSide';  
+            }); 
+        this.rightButton = this.add.image(500, 300, 'Right')
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => {
+                this.selectedSide = 'rightSide';  
+            }); 
+        //this.scene.start("playGame");
     }
     
     preload() {
 
+        this.load.image('Left', 'assets/GameSpritesL.png'); 
+        this.load.image('Right', 'assets/GameSpritesR.png'); 
+        this.load.image('LeftClick', 'assets/GameSpritesLC.png'); 
+        this.load.image('RightClick', 'assets/GameSpritesRC.png'); 
     }
 
     update() {
-
+        this.clickCountText.setText(`You have selected the ${this.selectedSide} Button`);
     }
-
 }
--- public/js/game.js
+++ public/js/game.js
@@ -2,6 +2,7 @@ let config = {
     width: 800,
     height: 600, 
     backgroundColor: 0x000000,
+
     scene: [Scene1, Scene2]
 }
 
