7a2a6400ec8ed8253972ba122f4c169fa5a9f195
Arjun-Khurana <35874188+Arjun-Khurana@users.noreply.github.com>
2019-11-09 22:17:28 -0600

    added movement for player

--- public/js/Scene2.js
+++ public/js/Scene2.js
@@ -7,16 +7,37 @@ class Scene2 extends Phaser.Scene {
         console.log("hello from scene 2");
     }
 
-    create() {
-
-    }
-    
     preload() {
-
+        this.load.image('star', '../assets/star.png')
+        this.load.image('bomb', '../assets/bomb.png')
     }
 
-    update() {
-        
+    create() {
+        this.player = this.impact.add.sprite(400,300, 'star');
+        this.player.setMaxVelocity(1000).setFriction(800, 600).setPassiveCollision();
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+      
+    update(time,delta) {
+        if (this.cursors.left.isDown) {
+            this.player.setAccelerationX(-800);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setAccelerationX(800);
+        }
+        else {
+            this.player.setAccelerationX(0);
+        }
+
+        if (this.cursors.down.isDown) {
+            this.player.setAccelerationY(800);
+        }
+        else if (this.cursors.up.isDown) {
+            this.player.setAccelerationY(-800);
+        }
+        else {
+            this.player.setAccelerationY(0);
+        }
     }
 
 }
